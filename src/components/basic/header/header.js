2296754eba96737c5e1ede9f14a009a4210c22e7
biswajitdazz <biswajit.bd94@gmail.com>
2021-08-15 09:35:24 -0700

    ok

---
+++ src/components/basic/header/header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './header.css'; 
+
+
+function HomepageHeader(props) {
+    return (
+        <div>
+            <div className="header-container-2">
+            </div>
+        </div>
+    );
+}
+
+export default HomepageHeader;
